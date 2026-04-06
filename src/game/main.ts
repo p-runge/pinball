3471a1a05de37a29eda04995c6979ab3b64afe34
Philipp Runge <p.runge@progani.com>
2026-04-06 14:05:13 +0200

    make canvas grow with viewport size

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,13 +1,17 @@
 import { Game as PinballGame } from "./scenes/Game";
 import { MainMenu } from "./scenes/MainMenu";
-import { AUTO, Game } from "phaser";
+import { AUTO, Game, Scale } from "phaser";
 
 const config: Phaser.Types.Core.GameConfig = {
   type: AUTO,
-  width: 480,
-  height: 840,
   parent: "game-container",
-  backgroundColor: "#000000",
+  backgroundColor: "#333333",
+  scale: {
+    mode: Scale.FIT,
+    autoCenter: Scale.CENTER_BOTH,
+    width: 480,
+    height: 840,
+  },
   scene: [MainMenu, PinballGame],
 };
 
